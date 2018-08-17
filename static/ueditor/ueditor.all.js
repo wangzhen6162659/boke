7ae63c15a4dcaa9fa41d50ce2c8ae239e373f757
wangzhen6162659 <1426640240@qq.com>
2018-08-17 10:31:57 +0800

    修复ueditor下拉问题

--- static/ueditor/ueditor.all.js
+++ static/ueditor/ueditor.all.js
@@ -17547,28 +17547,28 @@ UE.plugins['autofloat'] = function() {
         getPosition,
         flag =true;   //ie7模式下需要偏移
     function setFloating(){
-        var toobarBoxPos = domUtils.getXY(toolbarBox),
-            origalFloat = domUtils.getComputedStyle(toolbarBox,'position'),
-            origalLeft = domUtils.getComputedStyle(toolbarBox,'left');
-        toolbarBox.style.width = toolbarBox.offsetWidth + 'px';
-        toolbarBox.style.zIndex = me.options.zIndex * 1 + 1;
-        toolbarBox.parentNode.insertBefore(placeHolder, toolbarBox);
-        if (LteIE6 || (quirks && browser.ie)) {
-            if(toolbarBox.style.position != 'absolute'){
-                toolbarBox.style.position = 'absolute';
-            }
-            toolbarBox.style.top = (document.body.scrollTop||document.documentElement.scrollTop) - orgTop + topOffset  + 'px';
-        } else {
-            if (browser.ie7Compat && flag) {
-                flag = false;
-                toolbarBox.style.left =  domUtils.getXY(toolbarBox).x - document.documentElement.getBoundingClientRect().left+2  + 'px';
-            }
-            if(toolbarBox.style.position != 'fixed'){
-                toolbarBox.style.position = 'fixed';
-                toolbarBox.style.top = topOffset +"px";
-                ((origalFloat == 'absolute' || origalFloat == 'relative') && parseFloat(origalLeft)) && (toolbarBox.style.left = toobarBoxPos.x + 'px');
-            }
-        }
+        // var toobarBoxPos = domUtils.getXY(toolbarBox),
+        //     origalFloat = domUtils.getComputedStyle(toolbarBox,'position'),
+        //     origalLeft = domUtils.getComputedStyle(toolbarBox,'left');
+        // toolbarBox.style.width = toolbarBox.offsetWidth + 'px';
+        // toolbarBox.style.zIndex = me.options.zIndex * 1 + 1;
+        // toolbarBox.parentNode.insertBefore(placeHolder, toolbarBox);
+        // if (LteIE6 || (quirks && browser.ie)) {
+        //     if(toolbarBox.style.position != 'absolute'){
+        //         toolbarBox.style.position = 'absolute';
+        //     }
+        //     toolbarBox.style.top = (document.body.scrollTop||document.documentElement.scrollTop) - orgTop + topOffset  + 'px';
+        // } else {
+        //     if (browser.ie7Compat && flag) {
+        //         flag = false;
+        //         toolbarBox.style.left =  domUtils.getXY(toolbarBox).x - document.documentElement.getBoundingClientRect().left+2  + 'px';
+        //     }
+        //     if(toolbarBox.style.position != 'fixed'){
+        //         toolbarBox.style.position = 'fixed';
+        //         toolbarBox.style.top = topOffset +"px";
+        //         ((origalFloat == 'absolute' || origalFloat == 'relative') && parseFloat(origalLeft)) && (toolbarBox.style.left = toobarBoxPos.x + 'px');
+        //     }
+        // }
     }
     function unsetFloating(){
         flag = true;
