345686f8267ee4048c00ca0ad8d1e40a4412cf5e
wangzhen6162659 <1426640240@qq.com>
2018-10-12 17:17:22 +0800

    修改了网关端口

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,6 +2,6 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"http://192.168.1.124:9999"',//统一内部接口
+  API_ROOT: '"http://192.168.1.124:10000"',//统一内部接口
   API_MUSIC_ROOT: '"http://123.56.221.77"'//网易云api
 })
