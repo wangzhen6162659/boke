df45eeaa08c85a18523a4beff61de00ec67c4b95
wz6162659 <470382668@qq.com>
2018-10-23 11:36:57 +0800

    回退

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
-  // API_ROOT: '"http://wzroom.cn"',//统一内部接口
-  // API_MUSIC_ROOT: '"http://wzroom.cn"'//网易云api
-  API_ROOT: '"http://123.56.221.77"',//统一内部接口
-  API_MUSIC_ROOT: '"http://123.56.221.77"'//网易云api
+  API_ROOT: '"http://wzroom.cn"',//统一内部接口
+  API_MUSIC_ROOT: '"http://wzroom.cn"'//网易云api
+  // API_ROOT: '"http://123.56.221.77"',//统一内部接口
+  // API_MUSIC_ROOT: '"http://123.56.221.77"'//网易云api
 }
