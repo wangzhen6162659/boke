59c86447f2e2b27c8f7227c868f4b9425498101b
wangzhen6162659 <470382668@qq.com>
2018-10-29 22:45:17 +0800

    修复了歌名较长的时候无自适应播放设置

--- src/utils/wxUtile.js
+++ src/utils/wxUtile.js
@@ -43,7 +43,8 @@ export default {
       timestamp: data.timestamp, // 必填，生成签名的时间戳
       nonceStr: data.nonceStr, // 必填，生成签名的随机串
       signature: data.signature, // 必填，签名，见附录1
-      jsApiList: ['updateAppMessageShareData',
+      jsApiList: ['checkJsApi',
+        'updateAppMessageShareData',
         'updateTimelineShareData'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
     wx.checkJsApi({
