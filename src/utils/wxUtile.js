80d3f8206a3011d5804a31bc7610e3eaf32f93c0
wz6162659 <470382668@qq.com>
2018-10-29 16:41:50 +0800

    测试错误

--- src/utils/wxUtile.js
+++ src/utils/wxUtile.js
@@ -33,7 +33,7 @@ export default {
   wxConfig(data, shareData){
     alert(JSON.stringify(shareData))
     wx.config({
-      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: data.appId, // 必填，公众号的唯一标识
       timestamp: data.timestamp, // 必填，生成签名的时间戳
       nonceStr: data.nonceStr, // 必填，生成签名的随机串
@@ -48,6 +48,8 @@ export default {
         imgUrl: shareData.shareImg, // 分享图标
       }, function (res) {
         //这里是回调函数
+        alert(JSON.stringify(shareData))
+        alert('success');
       });
     });
   }
