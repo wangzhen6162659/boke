3187cef75d5b66655831b5f6a1c8581e1078eba6
wz6162659 <470382668@qq.com>
2018-10-29 14:48:13 +0800

    退回

--- src/utils/wxUtile.js
+++ src/utils/wxUtile.js
@@ -21,10 +21,10 @@ export default {
   wxShare(_this, shareTitle, shareUrl, shareImg, shareDesc) {
     // shareUrl = getWxUrl(shareUrl);
     fecth.get(apiList.getWxConfig, {url: 'updateAppMessageShareData'}).then((res) => {
-      let data = getAccessToken();
+      let data = res.data;
       wx.config({
         debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-        appId: data.appId, // 必填，公众号的唯一标识
+        appId: data.appId, // 必填，公众号的唯一标识ssss
         timestamp: data.timestamp, // 必填，生成签名的时间戳
         nonceStr: data.nonceStr, // 必填，生成签名的随机串
         signature: data.signature, // 必填，签名，见附录1
